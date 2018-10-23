f3edb79fb1e620b1cd71eb119f28397f90fea675
李春寅 <licy2013@aliyun.com>
2018-10-23 20:52:23 +0800

    bootstrap.php 修改为acomposer autoload没有加载时才会去加载它

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,19 @@
 <?php
+/**
+ * 如果composer提供的autoload已经加载，那么直接返回autoload对象。
+ */
+if(class_exists('asbamboo\\autoload\\Autoload')){
+    return new asbamboo\autoload\Autoload();
+}
+
+/**
+ * 加载composer自带的autoload
+ * 返回一个初始化Autoload
+ */
+if(file_exists(__DIR__ . '/vendor/autoload.php')){
+    require_once __DIR__ . '/vendor/autoload.php';
+}else{
+    require_once dirname(dirname(__DIR__)) . '/autoload.php';
+}
+
 return new asbamboo\autoload\Autoload();
