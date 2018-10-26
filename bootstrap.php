383edacd7175669b5bd6af878e4fabea557a9575
Lichunyin <licy2013@aliyun.com>
2018-10-26 14:23:54 +0800

    1.0.1

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,21 @@
 <?php
 /**
- * 如果composer提供的autoload已经加载，那么直接返回autoload对象。
- */
-if(class_exists('asbamboo\\autoload\\Autoload')){
-    return new asbamboo\autoload\Autoload();
-}
-
-/**
  * 加载composer自带的autoload
  * 返回一个初始化Autoload
+ * 如果composer提供的autoload已经加载，那么直接返回autoload对象。
  */
-if(file_exists(__DIR__ . '/vendor/autoload.php')){
-    require_once __DIR__ . '/vendor/autoload.php';
-}else{
-    require_once dirname(dirname(__DIR__)) . '/autoload.php';
+$vendor_dir = dirname(dirname(__DIR__));
+if(!class_exists('asbamboo\\autoload\\Autoload')){
+    if(file_exists(__DIR__ . '/vendor/autoload.php')){
+        require_once __DIR__ . '/vendor/autoload.php';
+        $vendor_dir = __DIR__ . '/vendor';
+    }else{
+        require_once dirname(dirname(__DIR__)) . '/autoload.php';
+    }
 }
-
-return new asbamboo\autoload\Autoload();
+$Autoload   = new asbamboo\autoload\Autoload();
+$psr4s      = include $vendor_dir . '/composer/autoload_psr4.php';
+foreach($psr4s AS $namespace => $dir){
+    $Autoload->addMappingDir($namespace, $dir);
+}
+return $Autoload;
